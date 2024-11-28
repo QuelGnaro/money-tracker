499b8865ad485bff7f7e02baceacd0fed370a0e0
QuelGnaro <quelgnaro@gmail.com>
2024-11-28 10:37:03 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Money Tracker',
+  webDir: 'www'
+};
+
+export default config;
