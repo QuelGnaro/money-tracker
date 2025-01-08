c6fd4820604fe3c4844666310656cf764c2568c3
QuelGnaro <quelgnaro@gmail.com>
2025-01-08 22:46:13 +0100

    feat new interface for default icons

---
+++ src/app/constants/default-icon-list.constant.ts
@@ -0,0 +1,23 @@
+import { IIcon } from "../interface/icon.interface";
+
+export const defaultIcons: IIcon[] = [
+  { label: "build", id: 1 },
+  { label: "aperture", id: 2 },
+  { label: "car-sport", id: 3 },
+  { label: "bug", id: 4 },
+  { label: "bluetooth", id: 5 },
+  { label: "beer", id: 6 },
+  { label: "dice", id: 7 },
+  { label: "flash", id: 8 },
+  { label: "ice-cream", id: 9 },
+  { label: "school", id: 10 },
+  { label: "wine", id: 11 },
+  { label: "ticket", id: 12 },
+  { label: "newspaper", id: 13 },
+  { label: "hardware-chip", id: 14 },
+  { label: "earth", id: 15 },
+  { label: "bowling-ball", id: 16 },
+  { label: "male-female", id: 17 },
+  { label: "speedometer", id: 18 },
+  { label: "skull", id: 19 },
+];
